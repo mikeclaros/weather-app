78d6ad428f72ae0919519a25b757300eb728b145
Mike Claros <mike.s.claros@gmail.com>
2022-08-17 14:56:24 -0700

    moved geoparser up to container component and removed geoparser component

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,20 +2,54 @@ import React, { useState, useEffect } from 'react'
 import { DayDisplay } from './DayDisplay'
 import { DaysDisplay } from './DaysDisplay'
 import { Graph } from './Graph'
-import { geoParse } from '../Utils/parseGeoLoc'
+import { GeoLoc } from './GeoLoc'
 
 
 
 export function Container() {
-    const [zipCode, setZipCode] = useState('')
+    const [zipcode, setZipcode] = useState('')
     const [weatherData, setWeatherData] = useState([])
+    useEffect(() => { searchZipcode(zipcode) }, [zipcode])
 
     function handleSubmit(e) {
         let zipcodeVal = e.target.zipcode.value
         e.preventDefault()
-        setZipCode(() => zipcodeVal)
-        let values = geoParse(zipcodeVal)
-        console.log(`values: ${values}`);
+        setZipcode(() => zipcodeVal)
+        console.log(`zipcode entered: ${zipcodeVal}`);
+    }
+
+    const searchZipcode = (input) => {
+        if (input != "" && input != undefined) {
+            let url = `https://geocode.maps.co/search?q=${input}`
+            const req = new XMLHttpRequest()
+            req.open('GET', url)
+            req.responseType = 'json'
+            req.send()
+
+            req.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    // document.getElementById("demo").innerHTML =
+                    //     this.responseText;
+                    let object = this.response[0]
+                    let lat = object.lat
+                    let lon = object.lon
+                    console.log(`lat: ${lat}, lon: ${lon}`)
+                    let weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true&timezone=auto`
+
+                    const nextReq = new XMLHttpRequest()
+                    nextReq.open('GET', weatherURL)
+                    nextReq.responseType = 'json'
+                    nextReq.send()
+
+                    nextReq.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                            console.log('JSON RECEIVED: ', this.response)
+                            setWeatherData(() => this.response)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     return (
@@ -24,7 +58,7 @@ export function Container() {
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <input name='zipcode' placeholder='Enter Zipcode'></input>
                 </form>
-                <DayDisplay />
+                {console.log('Weather data?: ', (weatherData.length != 0) ? weatherData : 'none')}
             </div>
             <div>
                 <DaysDisplay />
--- src/Components/DayDisplay.jsx
+++ src/Components/DayDisplay.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 export function DayDisplay() {
-
+    return (
+        <div>
+            {console.log('this is dayDisplay')}
+        </div>
+    )
 }
