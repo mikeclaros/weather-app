de15b0b28cef2b8db0b8a34122a61c5b1bb695e1
Mike Claros <mike.s.claros@gmail.com>
2022-08-18 18:47:24 -0700

    added style for days

--- src/Components/DaysDisplay.jsx
+++ src/Components/DaysDisplay.jsx
@@ -41,7 +41,7 @@ export function DaysDisplay({ value }) {
         let arr = days[id]
         return (
             <div id={day} key={day} className='days-style'>
-                <div className='div-sm-pd' id='Time'>{arr[0]}</div>
+                <div className='div-sm-pd background-entry' id='Time'>{arr[0]}</div>
                 <div className={'div-sm-pd ' + backgroundPicker(arr[1])} id='Max'>{arr[1]}</div>
                 <div className={'div-sm-pd ' + backgroundPicker(arr[2])} id='Min'>{arr[2]}</div>
             </div>
