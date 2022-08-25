2055620e51851aecae4c61d7864ac3852b1db392
Mike Claros <mike.s.claros@gmail.com>
2022-08-25 11:16:00 -0700

    added browser geo location alternative, modified div positioning

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -7,64 +7,113 @@ import { Graph } from './Graph'
 
 export function Container() {
     const [zipcode, setZipcode] = useState('')
+    const [geoObj, setGeoObj] = useState({})
     const [weatherData, setWeatherData] = useState([])
     useEffect(() => { searchZipcode(zipcode) }, [zipcode])
+    useEffect(() => { searchGeoObj(geoObj) }, [geoObj])
 
     function handleSubmit(e) {
-        let zipcodeVal = e.target.zipcode.value
-        e.preventDefault()
-        setZipcode(() => zipcodeVal)
-        console.log(`zipcode entered: ${zipcodeVal}`);
+
+        if (e.target.zipcode != '') {
+            let zipcodeVal = e.target.zipcode.value
+            e.preventDefault()
+            setZipcode(() => zipcodeVal)
+            console.log(`zipcode entered: ${zipcodeVal}`)
+        }
+    }
+
+    function handleGeoLocButtonClick(e) {
+        BrowserGeoCode.geocoder()
+    }
+
+    const BrowserGeoCode = {
+        geocoder: function () {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((pos) => {
+                    console.log('in geocoder', this)
+                    // this.lat = pos.coords.latitude
+                    // this.lon = pos.coords.longitude
+                    setGeoObj(() => ({ ...geoObj, lat: pos.coords.latitude, lon: pos.coords.longitude }))
+                })
+            }
+        },
+    }
+
+    const WeatherData = {
+        url: '',
+        req: new XMLHttpRequest(),
+        lat: NaN,
+        lon: NaN,
+        data: {},
+        getData: function () {
+            //let weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lon}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true&timezone=auto`
+            this.url = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lon}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true&timezone=auto`
+            this.req.open('GET', this.url)
+            this.req.responseType = 'json'
+            this.req.send()
+
+            this.req.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log('JSON RECEIVED: ', this.response)
+                    setWeatherData(() => this.response)
+                }
+            }
+        },
+    }
+
+    const searchGeoObj = (geoObj) => {
+        if (geoObj.lat != undefined && geoObj.lon != undefined) {
+            WeatherData.lat = geoObj.lat
+            WeatherData.lon = geoObj.lon
+            WeatherData.getData()
+        }
     }
 
     const searchZipcode = (input) => {
-        let geocodeDown = false //might put in a backup geocoder
-        if (geocodeDown) {
-            let url = `https://api.open-meteo.com/v1/forecast?latitude=34.125&longitude=-118.25&hourly=temperature_2m&temperature_unit=fahrenheit&daily=apparent_temperature_max,apparent_temperature_min&current_weather=true&timezone=auto`
+        console.log("Searching zipcode...")
+        let testing = true
+        if (input != "" && input != undefined && !testing) {
+            let url = `https://geocode.maps.co/search?q=${input}`
             const req = new XMLHttpRequest()
             req.open('GET', url)
             req.responseType = 'json'
+            let geocodeReady = true;
             req.send()
 
             req.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log('geocode used hardcoded weather url to continue developing');
-                    setWeatherData(() => this.response)
+                    let dataObj = new WeatherData()
+                    let lat = this.response[0].lat
+                    let lon = this.response[0].lon
+
+                    dataObj.lat = lat
+                    dataObj.lon = lon
+                    dataObj.getData()
                 }
-            }
-        } else {
-            if (input != "" && input != undefined) {
-                let url = `https://geocode.maps.co/search?q=${input}`
-                const req = new XMLHttpRequest()
-                req.open('GET', url)
-                req.responseType = 'json'
-                req.send()
+                // if (this.readyState == 4 && this.status == 200) {
+                //     // document.getElementById("demo").innerHTML =
+                //     //     this.responseText;
+                //     let object = this.response[0]
+                //     let lat = object.lat
+                //     let lon = object.lon
+                //     console.log(`lat: ${lat}, lon: ${lon}`)
+                //     let weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true&timezone=auto`
 
-                req.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        // document.getElementById("demo").innerHTML =
-                        //     this.responseText;
-                        let object = this.response[0]
-                        let lat = object.lat
-                        let lon = object.lon
-                        console.log(`lat: ${lat}, lon: ${lon}`)
-                        let weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&temperature_unit=fahrenheit&current_weather=true&timezone=auto`
+                //     const nextReq = new XMLHttpRequest()
+                //     nextReq.open('GET', weatherURL)
+                //     nextReq.responseType = 'json'
+                //     nextReq.send()
 
-                        const nextReq = new XMLHttpRequest()
-                        nextReq.open('GET', weatherURL)
-                        nextReq.responseType = 'json'
-                        nextReq.send()
-
-                        nextReq.onreadystatechange = function () {
-                            if (this.readyState == 4 && this.status == 200) {
-                                console.log('JSON RECEIVED: ', this.response)
-                                setWeatherData(() => this.response)
-                            }
-                        }
-                    }
-                }
-            } else {
-                console.log('no input found')
+                //     nextReq.onreadystatechange = function () {
+                //         if (this.readyState == 4 && this.status == 200) {
+                //             console.log('JSON RECEIVED: ', this.response)
+                //             setWeatherData(() => this.response)
+                //         }
+                //     }
+                // } else {
+                //     let obj = new BrowserGeoCode()
+                //     obj.geocoder()
+                // }
             }
         }
     }
@@ -72,13 +121,16 @@ export function Container() {
     return (
         <div className='min-width'>
             <div className='background-entry'>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <input name='zipcode' placeholder='Enter Zipcode'></input>
-                </form>
-                {console.log('Weather data?: ', (weatherData.length != 0) ? weatherData : 'none')}
-                <div>
-                    <DayDisplay value={weatherData.current_weather} />
+                <div className='div-entry-col'>
+                    <form onSubmit={(e) => handleSubmit(e)}>
+                        <input name='zipcode' placeholder='Enter Zipcode'></input>
+                    </form>
+                    <button className='div-sm-margin' name='geoloc' onClick={(e) => handleGeoLocButtonClick(e)}>Use GeoLocation?</button>
                 </div>
+            </div>
+            {console.log('Weather data?: ', (weatherData.length != 0) ? weatherData : 'none')}
+            <div>
+                <DayDisplay value={weatherData.current_weather} />
             </div>
             <div>
                 <Graph value={weatherData.hourly} />
